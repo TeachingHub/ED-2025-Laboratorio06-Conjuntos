5c2516f61090ad8de7d1ebb2d97d288c84c1c8e1
Sergio Cavero <sergio.cavero@urjc.es>
2025-03-25 12:31:50 +0100

    Add initial implementation of conjunto data structure and associated test cases

---
+++ uMiConjunto.pas
@@ -0,0 +1,32 @@
+unit uMiConjunto;
+
+interface
+
+uses uListaEnlazadaSimple;
+
+type
+    tConjunto = record
+        lista: tListaSimple;
+    end;
+
+    {Operaciones básicas}
+    procedure init(var c: tConjunto);
+    procedure add(var c: tConjunto; x: integer);
+    procedure remove(var c: tConjunto; x: integer);
+    function contains(c: tConjunto; x: integer): boolean;
+    function is_empty_con(c: tConjunto): boolean;
+    function size(c: tConjunto): integer;
+    function to_string_con(c: tConjunto): string;
+    procedure empty(var c: tConjunto);
+
+    {Otras operaciones}
+    procedure union(c1, c2: tConjunto; var c3: tConjunto);
+    procedure intersection(c1, c2: tConjunto; var c3: tConjunto);
+    procedure difference(c1, c2: tConjunto; var c3: tConjunto);
+
+
+implementation
+
+
+
+end.
